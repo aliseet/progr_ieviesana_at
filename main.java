be63e98b8c454a4e4df1e45a2822d1c5156810e1
aliseet <trifanovaalise@gmail.com>
2026-09-15 15:09:18 +0300

    izveidoju klasi, objektu un mainīgo
    izveidoju klasi, izveidoju vienu objektu kurš lasa lietotāja inputu no klavieratūras, un izveidoju vienu mainīgo

--- main.java
+++ main.java
@@ -1,2 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
+public class ShoppingCartSystem {
+    public static Scanner scanner = new Scanner(System.in);
+    static int choice;
+    public ArrayList<Item> cart = new ArrayList<>();
+
+    
+}
