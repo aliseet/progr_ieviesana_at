aba2f3c1f227ae8a8378e388761093fd6f982ef9
aliseet <trifanovaalise@gmail.com>
2026-09-15 15:17:18 +0300

    izveidot loop
    Izveidoju loop priekš izvēlnes

--- main.java
+++ main.java
@@ -7,6 +7,17 @@ public class ShoppingCartSystem {
     public ArrayList<Item> cart = new ArrayList<>();
 
     public static void main(String[] args) {
-        
+        do {
+            System.out.println("Shopping Cart System: ");
+            System.out.println("1. Add Item (Name, Price)");
+            System.out.println("2. Add Item (Name, Price, Quantity");
+            System.out.println("3. Add Item (Name, Price, Quantity, Discount");
+            System.out.println("4. View Cart");
+            System.out.println("5. Calculate Total");
+            System.out.println("6. Exit");
+            System.out.println("\nEnter an option; ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+        } while (choice != 6);
     }
 }
